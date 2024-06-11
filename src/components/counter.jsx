68e67d2c7827b46f417cb663a448e8f971699472
Gerard Cami <gerard.camig@educem.net>
2024-06-11 15:27:04 +0200

    View transitions and preact implementation

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import { useState } from "preact/hooks";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
