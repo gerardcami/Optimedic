d59fb8c36018246bf6bc2c3cbd49187de282749e
gcami <gerardcamigay@gmail.com>
2024-08-12 10:59:49 +0200

    started writing the file with the loupes data

---
+++ src/services/loupes-data.ts
@@ -0,0 +1,49 @@
+import { Description } from "@headlessui/react";
+
+export const loupesData = [
+  {
+    type: "Aumento fijo",
+    products: [
+      {
+        name: "RDH Elite™ 2,5x",
+        description:
+          "Una lupa higiénica con aumento de 2,5x centrada en mejorar la ergonomía y la productividad, diseñada teniendo en cuenta a higienistas, para higienistas.",
+      },
+      {
+        name: "HDL™ 2.5 Micro",
+        description:
+          "Óptica de tamaño micro que ofrece una comodidad ligera sin sacrificar el aumento.",
+      },
+      {
+        name: "HDL™ 2.5 Macro",
+        description:
+          "Las pruebas realizadas por terceros confirman que nuestras lupas ofrecen la calidad de imagen más clara del sector, con la mejor calidad de resolución óptica y transmisión de luz.",
+      },
+      {
+        name: "HDL™ 3.0",
+        description:
+          "Gracias a su diseño óptico, HDL es una de las lupas de 3,0 aumentos más ligeras que existen.",
+      },
+      {
+        name: "HDL™ 3.5x",
+        description:
+          "Aumento de mayor precisión diseñado con un amplio campo de visión.",
+      },
+      {
+        name: "HDL™ 4.5x",
+        description:
+          "Aumento de mayor precisión diseñado con un amplio campo de visión.",
+      },
+      {
+        name: "HDL™ 5.5x",
+        description:
+          "Nuestro aumento independiente más alto diseñado para brindar precisión y rendimiento.",
+      },
+      {
+        name: "OmniOptic™",
+        description:
+          "Descubra la primera y única lupa de aumento intercambiable que permite a los clínicos seleccionar el poder de ampliación óptimo para cada procedimiento. ",
+      },
+    ],
+  },
+];
