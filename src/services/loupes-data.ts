3474fca6fa830e64c11549bb52b1536e7db1dffb
gcami <gerardcamigay@gmail.com>
2024-08-12 23:17:11 +0200

    added the cover images to the loupes data file. Finished the Loupes title section component.

--- src/services/loupes-data.ts
+++ src/services/loupes-data.ts
@@ -6,43 +6,93 @@ export const loupesData = [
     products: [
       {
         name: "RDH Elite™ 2,5x",
+        coverImage: "/public/img/loupes/covers/RDH_Elite25x.jpg",
         description:
           "Una lupa higiénica con aumento de 2,5x centrada en mejorar la ergonomía y la productividad, diseñada teniendo en cuenta a higienistas, para higienistas.",
       },
       {
         name: "HDL™ 2.5 Micro",
+        coverImage: "/public/img/loupes/covers/HDL25Micro.jpg",
         description:
           "Óptica de tamaño micro que ofrece una comodidad ligera sin sacrificar el aumento.",
       },
       {
         name: "HDL™ 2.5 Macro",
+        coverImage: "/public/img/loupes/covers/HDL25Macro.jpg",
         description:
           "Las pruebas realizadas por terceros confirman que nuestras lupas ofrecen la calidad de imagen más clara del sector, con la mejor calidad de resolución óptica y transmisión de luz.",
       },
       {
         name: "HDL™ 3.0",
+        coverImage: "/public/img/loupes/covers/RDH_Elite25x.jpg",
         description:
           "Gracias a su diseño óptico, HDL es una de las lupas de 3,0 aumentos más ligeras que existen.",
       },
       {
         name: "HDL™ 3.5x",
+        coverImage: "/public/img/loupes/covers/HDL35x.jpg",
         description:
           "Aumento de mayor precisión diseñado con un amplio campo de visión.",
       },
       {
         name: "HDL™ 4.5x",
+        coverImage: "/public/img/loupes/covers/HDL45x.jpg",
         description:
           "Aumento de mayor precisión diseñado con un amplio campo de visión.",
       },
       {
         name: "HDL™ 5.5x",
+        coverImage: "/public/img/loupes/covers/HDL55x.jpg",
         description:
           "Nuestro aumento independiente más alto diseñado para brindar precisión y rendimiento.",
       },
       {
         name: "OmniOptic™",
+        coverImage: "/public/img/loupes/covers/OmniOptic.jpg",
         description:
           "Descubra la primera y única lupa de aumento intercambiable que permite a los clínicos seleccionar el poder de ampliación óptimo para cada procedimiento. ",
+      },
+    ],
+  },
+  {
+    type: "Aumento variable",
+    products: [
+      {
+        name: "EyeZoom™ Mini",
+        coverImage: "/public/img/loupes/covers/EyeZoom_Mini.jpg",
+        description:
+          "Cambie sin problemas entre 2,5x y 3,5x con nuestra galardonada lupa de aumento variable.",
+      },
+      {
+        name: "EyeZoom™",
+        coverImage: "/public/img/loupes/covers/EyeZoom.jpg",
+        description:
+          "La primera y única lupa de aumento variable 3 en 1. Cambie sin problemas entre los niveles de aumento 3x, 4x y 5x.",
+      },
+      {
+        name: "EyeZoom™ Max",
+        coverImage: "/public/img/loupes/covers/EyeZoom_Max.jpg",
+        description: "TODO.",
+      },
+    ],
+  },
+  {
+    type: "Ergo",
+    products: [
+      {
+        name: "HDL™ Ergo 3.0x",
+        coverImage: "/",
+        description: "TODO.",
+      },
+      {
+        name: "HDL™ Ergo 3.5x",
+        coverImage: "/public/img/loupes/covers/HDL_Ergo35x.jpg",
+        description: "TODO.",
+      },
+      {
+        name: "HDL Ergo™ 5.0x",
+        coverImage: "",
+        description: "Nuestro mayor aumento Ergo",
       },
     ],
   },
