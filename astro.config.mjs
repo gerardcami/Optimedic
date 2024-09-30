b9bec6dcc0a1b21a462d0391dc90ce642a27af83
gcami <gerardcamigay@gmail.com>
2024-09-30 13:37:59 +0200

    Implementing i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid'
+  output: 'hybrid',
+  i18n: {
+    defaultLocale: 'es', // Idioma por defecto
+    locales: ['es', 'ca'], // Idiomas locales
+    routing: {
+      prefixDefaultLocale: false // true: es -> /es  ca -> /ca (cambia la url)
+    }
+  }
 });
