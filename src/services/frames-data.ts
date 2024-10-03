62adb14ca7f9cd0ccfae1287f524a92bd5f6a375
gcami <gerardcamigay@gmail.com>
2024-10-03 19:49:36 +0200

    Images of the frames and all related information have been added.

---
+++ src/services/frames-data.ts
@@ -0,0 +1,450 @@
+export const framesData = [
+  {
+    type: "Deportiva",
+    products: [
+      {
+        name: "Rydon™",
+        slug: "rydon",
+        card_image: "/public/img/frames/rydon/card.webp",
+        description:
+          "Lleve la personalización a un nuevo nivel con bisagras de patillas flexibles, patillas plegables y almohadillas nasales ajustables.",
+        details: [
+          {
+            detailed_description:
+              "Esta clásica y cómoda montura deportiva ofrece un ajuste personalizado y elegante con bisagras de patillas flexibles, los brazos de las patillas plegables y almohadillas nasales ajustables",
+            intro_text_1:
+              "Diseñadas y fabricadas por el famoso icono de las gafas Rudy Project®, la montura Rydon™ ofrece un ajuste verdaderamente a medida para cada usuario. Con las bisagras flexibles, las terminales de las patillas flexibles y las plaquetas nasales ajustables, esta montura lleva la personalización a un nuevo nivel.",
+            intro_text_2:
+              "La construcción exclusiva de semibordeado admite también dos formas de lente de soporte diferentes, lo que permite adaptar más la lupa al usuario al acomodar ángulos de declinación pronunciados y geometrías faciales específicas.",
+            intro_text_3:
+              "La Rydon admite una amplia gama de prescripciones incluidas las bifocales.",
+            colors: [
+              {
+                name: "Negro",
+                image: "/public/img/frames/rydon/black.png",
+              },
+              {
+                name: "Azul",
+                image: "/public/img/frames/rydon/blue.png",
+              },
+              {
+                name: "Carbono",
+                image: "/public/img/frames/rydon/carbon.png",
+              },
+              {
+                name: "Merlot",
+                image: "/public/img/frames/rydon/merlot.png",
+              },
+              {
+                name: "Carey",
+                image: "/public/img/frames/rydon/carey.png",
+              },
+            ],
+            specs: [
+              {
+                materials:
+                  "Carbono, magnesio, silicio, grilamida y titanio avanzados",
+                sizes: "Tamaño único",
+              },
+            ],
+            loupe_options: [
+              {
+                name: "HDL™ 2.5 Micro",
+                slug: "hdl-25-micro",
+              },
+              {
+                name: "HDL™ 2.5 Macro",
+                slug: "hdl-25-macro",
+              },
+              {
+                name: "HDL™ 3.0",
+                slug: "hdl-30",
+              },
+              {
+                name: "EyeZoom™ Mini",
+                slug: "eyezoom-mini",
+              },
+              {
+                name: "EyeZoom™",
+                slug: "eyezoom",
+              },
+              {
+                name: "HDL Ergo™ 5.0x",
+                slug: "hdl-ergo-50x",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Phantom™",
+        slug: "phantom",
+        card_image: "/public/img/frames/phantom/card.webp",
+        description:
+          "Phantom, fabricada en titanio ligero, es la montura deportiva con graduación incorporada más ligera del mercado.",
+        details: [
+          {
+            detailed_description:
+              "Comodidad ligera sin comprometer el rendimiento.",
+            intro_text_1:
+              "Fabricada en titanio ligero, Phantom es la montura deportiva con graduación incorporada más ligera del mercado. Phantom es un 15% más ligera que la principal competidora, con graduación.",
+            intro_text_2:
+              "Todas las mejores características - puntas de patilla flexibles, puente nasal ajustable, canal de control de cables y bisagras flexibles - sin el peso añadido.",
+            intro_text_3:
+              "Disponible en cinco colores impresionantes, Phantom es la elección perfecta para quienes priorizan la comodidad y el estilo.",
+            colors: [
+              {
+                name: "Negro",
+                image: "/public/img/frames/phantom/black.png",
+              },
+              {
+                name: "Iris",
+                image: "/public/img/frames/phantom/iris.png",
+              },
+              {
+                name: "Oro rosa",
+                image: "/public/img/frames/phantom/rosegold.png",
+              },
+              {
+                name: "Zafiro",
+                image: "/public/img/frames/phantom/sapphire.png",
+              },
+              {
+                name: "Pizarra",
+                image: "/public/img/frames/phantom/slate.png",
+              },
+            ],
+            specs: [
+              {
+                materials: "Titanio",
+                sizes: "Pequeño · Mediano",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "Icónica",
+    products: [
+      {
+        name: "Triumph™",
+        slug: "triumph",
+        card_image: "/public/img/frames/triumph/card.webp",
+        description:
+          "Esta montura icónica emplea una función de control de cables incorporada y está disponible en cinco impresionantes colores bicolores.",
+        details: [
+          {
+            detailed_description:
+              "Esta montura icónica emplea una función integrada de control por cable y admite todos los niveles de aumento.",
+            intro_text_1:
+              "Combinando rendimiento y comodidad, Triumph™ es una ligera montura de titanio construida exclusivamente para lupas. El control por cable integrado elimina las distracciones y optimiza la eficacia. Elija cualquier nivel de aumento e incorpore su estilo personal con una de las cinco combinaciones de colores bicolores de moda. Las patillas ajustables y el puente nasal permiten un ajuste personalizado y una mayor comodidad.",
+            intro_text_2:
+              "Triumph™ está disponible en tamaño pequeño, mediano y grande y admite todo tipo de graduaciones.",
+            colors: [
+              {
+                name: "Negro",
+                image: "/public/img/frames/triumph/black.png",
+              },
+              {
+                name: "Dorado",
+                image: "/public/img/frames/triumph/gold.png",
+              },
+              {
+                name: "Oro rosa",
+                image: "/public/img/frames/triumph/rosegold.png",
+              },
+              {
+                name: "Zafiro",
+                image: "/public/img/frames/triumph/sapphire.png",
+              },
+              {
+                name: "Pizarra",
+                image: "/public/img/frames/triumph/slate.png",
+              },
+            ],
+            specs: [
+              {
+                materials: "Titanio",
+                sizes: "Pequeño · Mediano · Grande",
+              },
+            ],
+            loupe_options: [
+              {
+                name: "HDL™ 2.5 Micro",
+                slug: "hdl-25-micro",
+              },
+              {
+                name: "HDL™ 2.5 Macro",
+                slug: "hdl-25-macro",
+              },
+              {
+                name: "HDL™ 3.0",
+                slug: "hdl-30",
+              },
+              {
+                name: "OmniOptic™",
+                slug: "omnioptic",
+              },
+              {
+                name: "EyeZoom™ Mini",
+                slug: "eyezoom-mini",
+              },
+              {
+                name: "EyeZoom™",
+                slug: "eyezoom",
+              },
+              {
+                name: "EyeZoom™ Max",
+                slug: "eyezoom-max",
+              },
+              {
+                name: "HDL™ Ergo 3.5x",
+                slug: "hdl-ergo-35x",
+              },
+              {
+                name: "HDL Ergo™ 5.0x",
+                slug: "hdl-ergo-50x",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Tempo™",
+        slug: "tempo",
+        card_image: "/public/img/frames/tempo/card.webp",
+        description:
+          "Esta montura icónica aporta un diseño elegante y minimalista para el clínico moderno de hoy en día.",
+        details: [
+          {
+            detailed_description:
+              "Esta montura icónica aporta un diseño elegante y minimalista que admite todos los niveles de aumento.",
+            intro_text_1:
+              "Fabricada pensando en el rendimiento, la construcción ligera de titanio de Tempo™ es lo suficientemente duradera como para soportar la línea completa de lupas y frontales Orascoptic. La incorporación de patillas ajustables, un canal de control de cables y una almohadilla nasal en forma de silla de montar también permite un ajuste personalizable y una mayor comodidad.",
+            intro_text_2:
+              "Tempo™ is available in small, medium, and large and supports all prescription types.",
+            colors: [
+              {
+                name: "Amatista",
+                image: "/public/img/frames/tempo/amethyst.png",
+              },
+              {
+                name: "Negro",
+                image: "/public/img/frames/tempo/black.png",
+              },
+              {
+                name: "Azul",
+                image: "/public/img/frames/tempo/blue.png",
+              },
+              {
+                name: "Merlot",
+                image: "/public/img/frames/tempo/merlot.png",
+              },
+              {
+                name: "Pizarra",
+                image: "/public/img/frames/tempo/slate.png",
+              },
+            ],
+            specs: [
+              {
+                materials: "Titanio",
+                sizes: "Pequeño · Mediano · Grande",
+              },
+            ],
+            loupe_options: [
+              {
+                name: "HDL™ 2.5 Micro",
+                slug: "hdl-25-micro",
+              },
+              {
+                name: "HDL™ 2.5 Macro",
+                slug: "hdl-25-macro",
+              },
+              {
+                name: "HDL™ 3.0",
+                slug: "hdl-30",
+              },
+              {
+                name: "OmniOptic™",
+                slug: "omnioptic",
+              },
+              {
+                name: "EyeZoom™ Mini",
+                slug: "eyezoom-mini",
+              },
+              {
+                name: "EyeZoom™",
+                slug: "eyezoom",
+              },
+              {
+                name: "EyeZoom™ Max",
+                slug: "eyezoom-max",
+              },
+              {
+                name: "HDL™ Ergo 3.5x",
+                slug: "hdl-ergo-35x",
+              },
+              {
+                name: "HDL Ergo™ 5.0x",
+                slug: "hdl-ergo-50x",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Tempo™ Refined Fit",
+        slug: "tempo-refined-fit",
+        card_image: "/public/img/frames/tempo-re/card.webp",
+        description:
+          "Esta montura de ajuste refinado se ha diseñado para proporcionar una comodidad óptima a los clínicos con pómulos prominentes o un puente nasal bajo.",
+        details: [
+          {
+            detailed_description:
+              "Tempo™ Refined fit. Porque las lupas deben adaptarse cómodamente a todo el mundo.",
+            intro_text_1:
+              "Diga adiós a las incómodas lupas que se deslizan por la nariz o se apoyan en las mejillas. Esta versión optimizada de nuestra popular montura Tempo™ se ha diseñado para adaptarse cómodamente a los clínicos que tienen un puente nasal más bajo y pómulos prominentes.",
+            intro_text_2:
+              "Diseñado con un exclusivo alambre de puente nasal para permitir un ajuste cómodo y seguro en un puente nasal menos pronunciado, Tempo™ Refined Fit se diseñó con la altura de montura ideal para evitar la desalineación de la óptica causada por monturas apoyadas en los pómulos. ",
+            intro_text_3:
+              "Tempo™ Refined Fit está disponible en las tallas refinadas pequeña, mediana y grande y admite todo tipo de prescripciones.",
+            colors: [
+              {
+                name: "Amatista",
+                image: "/public/img/frames/tempo-re/amethyst.png",
+              },
+              {
+                name: "Negro",
+                image: "/public/img/frames/tempo-re/black.png",
+              },
+              {
+                name: "Azul",
+                image: "/public/img/frames/tempo-re/blue.png",
+              },
+              {
+                name: "Merlot",
+                image: "/public/img/frames/tempo-re/merlot.png",
+              },
+              {
+                name: "Pizarra",
+                image: "/public/img/frames/tempo-re/slate.png",
+              },
+            ],
+            specs: [
+              {
+                materials: "Titanio",
+                sizes: "Pequeño · Mediano · Grande",
+              },
+            ],
+            loupe_options: [
+              {
+                name: "HDL™ 2.5 Micro",
+                slug: "hdl-25-micro",
+              },
+              {
+                name: "HDL™ 2.5 Macro",
+                slug: "hdl-25-macro",
+              },
+              {
+                name: "HDL™ 3.0",
+                slug: "hdl-30",
+              },
+              {
+                name: "OmniOptic™",
+                slug: "omnioptic",
+              },
+              {
+                name: "EyeZoom™ Mini",
+                slug: "eyezoom-mini",
+              },
+              {
+                name: "EyeZoom™",
+                slug: "eyezoom",
+              },
+              {
+                name: "EyeZoom™ Max",
+                slug: "eyezoom-max",
+              },
+              {
+                name: "HDL™ Ergo 3.5x",
+                slug: "hdl-ergo-35x",
+              },
+              {
+                name: "HDL Ergo™ 5.0x",
+                slug: "hdl-ergo-50x",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Victory™",
+        slug: "victory",
+        card_image: "/public/img/frames/victory/card.webp",
+        description:
+          "Inspirada en la moda europea, esta montura tiene un perfil pequeño pero sofisticado.",
+        details: [
+          {
+            detailed_description:
+              "Fabricada en titanio, la montura Victory™ admite todas las graduaciones y resulta atractiva para los clínicos que prefieren gafas pequeñas y compactas. ",
+            intro_text_1:
+              "La pequeña y sofisticada montura Victory™ se inspira en la moda europea. Su pequeño perfil y su fiable estructura de titanio la convierten en una elección muy popular entre los médicos que buscan una montura de lupa ligera. ",
+            colors: [
+              {
+                name: "Negro",
+                image: "/public/img/frames/victory/black.png",
+              },
+              {
+                name: "Azul",
+                image: "/public/img/frames/victory/blue.png",
+              },
+              {
+                name: "Merlot",
+                image: "/public/img/frames/victory/merlot.png",
+              },
+
+              {
+                name: "Pizarra",
+                image: "/public/img/frames/victory/slate.png",
+              },
+            ],
+            specs: [
+              {
+                materials: "Titanio",
+                sizes: "Pequeño · Mediano · Grande",
+              },
+            ],
+            loupe_options: [
+              {
+                name: "HDL™ 2.5 Micro",
+                slug: "hdl-25-micro",
+              },
+              {
+                name: "HDL™ 2.5 Macro",
+                slug: "hdl-25-macro",
+              },
+              {
+                name: "HDL™ 3.0",
+                slug: "hdl-30",
+              },
+              {
+                name: "OmniOptic™",
+                slug: "omnioptic",
+              },
+              {
+                name: "EyeZoom™ Mini",
+                slug: "eyezoom-mini",
+              },
+              {
+                name: "EyeZoom™",
+                slug: "eyezoom",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
