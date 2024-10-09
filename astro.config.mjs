56df826ef3c4ee2062d5399a0446fa24c98c447c
gcami <gerardcamigay@gmail.com>
2024-10-09 17:25:10 +0200

    Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   i18n: {
-    defaultLocale: 'es', // Idioma por defecto
-    locales: ['es', 'ca', 'en'], // Idiomas locales
+    defaultLocale: 'es',
+    // Idioma por defecto
+    locales: ['es', 'ca', 'en'],
+    // Idiomas locales
     routing: {
       prefixDefaultLocale: false // true: es -> /es  ca -> /ca (cambia la url)
     }
   },
   devToolbar: {
     enabled: false
-  }
+  },
+  adapter: node({
+    mode: "standalone"
+  })
 });
