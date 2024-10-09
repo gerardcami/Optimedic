4ccbe1a764760bc303a5ed337627819c7b76e266
gcami <gerardcamigay@gmail.com>
2024-10-10 01:03:25 +0200

    Maintitle ready to add background image

--- src/components/LanguageDropdown.jsx
+++ src/components/LanguageDropdown.jsx
@@ -36,7 +36,7 @@ const LanguageDropdown = () => {
 
   return (
     <Popover className="relative inline-block text-left">
-      <Popover.Button className="inline-flex w-24 justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <Popover.Button className="inline-flex w-24 justify-between rounded-md px-4 py-2 text-sm font-medium border text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2">
         <div className="flex items-center">
           <img
             src={languages.find((lang) => lang.code === selectedLanguage)?.svg}
