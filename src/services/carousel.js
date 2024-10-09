e10e63966395188889b4457762bb2d91e28a3ebb
gcami <gerardcamigay@gmail.com>
2024-10-09 18:11:03 +0200

    images not loading test

--- src/services/carousel.js
+++ src/services/carousel.js
@@ -2,22 +2,22 @@ export const Slides = [
   {
     id: 1,
     image:
-      "/public/img/barcelona-oculoplastics-resumen-grafico-sorteo-gafas-lupa.jpg",
+      "/img/barcelona-oculoplastics-resumen-grafico-sorteo-gafas-lupa.jpg",
   },
   {
     id: 2,
-    image: "/public/img/oscar-alonso1.jpg",
+    image: "/img/oscar-alonso1.jpg",
   },
   {
     id: 3,
-    image: "/public/img/IMG_0345.png",
+    image: "/img/IMG_0345.png",
   },
   {
     id: 4,
-    image: "/public/img/IMG_0415.png",
+    image: "/img/IMG_0415.png",
   },
   {
     id: 5,
-    image: "/public/img/oscar-alonso2.jpg",
+    image: "/img/oscar-alonso2.jpg",
   },
 ];
