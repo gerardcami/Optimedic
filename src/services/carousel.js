d622ad60dde96d2d61afd659b043877e0050e376
gcami <gerardcamigay@gmail.com>
2024-10-16 15:44:54 +0200

    New images added to the carousel and cover updated.

--- src/services/carousel.js
+++ src/services/carousel.js
@@ -2,22 +2,46 @@ export const Slides = [
   {
     id: 1,
     image:
-      "/img/barcelona-oculoplastics-resumen-grafico-sorteo-gafas-lupa.jpg",
+      "/img/slides/slide1.jpg",
   },
   {
     id: 2,
-    image: "/img/oscar-alonso1.jpg",
+    image: "/img/slides/slide2.jpg",
   },
   {
     id: 3,
-    image: "/img/IMG_0345.png",
+    image: "/img/slides/slide3.jpg",
   },
   {
     id: 4,
-    image: "/img/IMG_0415.png",
+    image: "/img/slides/slide4.jpg",
   },
   {
     id: 5,
-    image: "/img/oscar-alonso2.jpg",
+    image: "/img/slides/slide5.jpg",
+  },
+  {
+    id: 6,
+    image: "/img/slides/slide6.jpg",
+  },
+  {
+    id: 7,
+    image: "/img/slides/slide7.jpg",
+  },
+  {
+    id: 8,
+    image: "/img/slides/slide8.jpg",
+  },
+  {
+    id: 9,
+    image: "/img/slides/slide9.jpg",
+  },
+  {
+    id: 10,
+    image: "/img/slides/slide10.jpg",
+  },
+  {
+    id: 11,
+    image: "/img/slides/slide11.jpg",
   },
 ];
