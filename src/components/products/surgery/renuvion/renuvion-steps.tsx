9cb7f62cb1d31d69974dd2fd16361c586bad8dd2
Gerard Camí <gerardcamigay@gmail.com>
2025-03-12 15:15:39 +0100

    Add BlurText animation component and enhance Renuvion layout and styles

--- src/components/products/surgery/renuvion/renuvion-steps.tsx
+++ src/components/products/surgery/renuvion/renuvion-steps.tsx
@@ -10,7 +10,7 @@ type RenuvionStepProps = {
 
 const RenuvionSteps = ({ content }: RenuvionStepProps) => {
 	return (
-		<section className="max-w-[700px] lg:max-w-full lg:px-6 xl:px-0">
+		<section className="max-w-[700px] lg:max-w-[1200px] lg:px-6 xl:px-0">
 			<h1 className="sr-only">Pasos renuvion</h1>
 			<div className="grid grid-cols-1 gap-4 px-6 lg:grid-cols-3 lg:px-0">
 				{content.map(({ step, title, description }) => (
